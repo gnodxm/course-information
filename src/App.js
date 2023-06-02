8d13d49f319fba48dc00cbe809f71f0df3dd13df
Gnod <gnodxm@gmail.com>
2023-06-02 10:19:39 +0700

    remove unneccesary file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = (props) => {
+	return (
+		<div>
+			<h1>
+				{props.course}
+			</h1>
+		</div>
+	)
+}
+
+const Part = (props) => {
+	console.log(props.exercies)
+	return(
+		<div>
+			<p>{props.name} {props.exercies}</p>
+		</div>
+	)
+}
+
+const Content = (props) => {
+	
+	return (
+		<div>
+			<Part name={props.parts[0].name} exercies={props.parts[0].exercises} />
+			<Part name={props.parts[1].name} exercies={props.parts[1].exercises} />
+			<Part name={props.parts[2].name} exercies={props.parts[2].exercises} />
+		</div>
+	)
+}
+
+const Total = (props) => {
+	return (
+		<div>
+			<p>Number of exercies {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+		</div>
+	)
+}
 
 function App() {
+	const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header course={course.name} />
+		 <Content parts={course.parts}		 />
+		 <Total parts={course.parts}		 />
     </div>
   );
 }
